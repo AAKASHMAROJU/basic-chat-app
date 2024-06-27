1feffd9e1787ecb8d3c5d77720f225ded77b7b67
aakash <marojuaakash2003@gmail.com>
2024-06-27 08:24:02 +0530

    edit and delete functionalities added

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const path = require("path");
 const port = 3000;
 const Chat = require("./models/chat");
 const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+const bodyParser = require("body-parser");
 
 main()
   .then(() => {
@@ -20,9 +22,11 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
+app.use(methodOverride("_method"));
 
 // For parsing application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Hello");
@@ -58,14 +62,38 @@ app.get("/chats/:id/edit", (req, res) => {
   const { id } = req.params;
   Chat.findOne({ _id: id })
     .then((data) => {
-      console.log(data);
-      const { from, to, msg, create_at } = data;
-      res.render("editForm", { from, to, msg, created_at });
+      console.log("Hello");
+      const { from, to, msg, created_at } = data;
+      res.render("editForm", {
+        from: from,
+        to: to,
+        msg: msg,
+        created_at: created_at,
+        id: id,
+      });
     })
     .catch((err) => {
       res.send(err);
     });
   // res.send("Hello");
+});
+
+app.put("/chats", (req, res) => {
+  console.log(req.body);
+  const { id, msg } = req.body;
+  Chat.updateOne({ _id: id }, { $set: { msg: msg } })
+    .then((m) => console.log(m))
+    .catch((err) => console.log(err));
+  res.redirect("/chats");
+});
+
+app.delete("/:id/delete", (req, res) => {
+  // console.log(req.params);
+  const { id } = req.params;
+  Chat.findByIdAndDelete({ _id: id })
+    .then((r) => console.log(r))
+    .catch((e) => console.log(e));
+  res.redirect("/chats");
 });
 
 app.listen(port, () => {
