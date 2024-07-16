4c57e1246b569ca2cfe499ccca54df419c298e92
aakashmaroju <marojuaakash2003@gmail.com>
2024-07-16 08:24:51 +0530

    added Error Handling

---
+++ ExpressError.js
@@ -0,0 +1,8 @@
+class ExpressError {
+  constructor(status, message) {
+    this.status = status;
+    this.message = message;
+  }
+}
+
+module.exports = ExpressError;
--- index.js
+++ index.js
@@ -7,12 +7,13 @@ const Chat = require("./models/chat");
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const bodyParser = require("body-parser");
+const ExpressError = require("./ExpressError");
 
 main()
   .then(() => {
     console.log("mongodb Connected Successfully");
   })
-  .catch((err) => console.log(err));
+  .catch((err) => next(err));
 
 async function main() {
   await mongoose.connect("mongodb://localhost:27017/whatsapp-db");
@@ -28,36 +29,41 @@ app.use(methodOverride("_method"));
 // app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
+app.get("/", (req, res, next) => {
   res.send("Hello");
 });
 
-app.get("/chats", async (req, res) => {
-  let data = await Chat.find();
-  // console.log(data);
-  res.render("index", { chats: data });
+app.get("/chats", async (req, res, next) => {
+  try {
+    let data = await Chat.find();
+    // console.log(data);
+    res.render("index", { chats: data });
+  } catch (err) {
+    next(err);
+  }
   // res.send("Working it is");
 });
 
-app.get("/chats/new", (req, res) => {
+app.get("/chats/new", (req, res, next) => {
   res.render("createChat");
 });
 
-app.post("/chats", async (req, res) => {
+app.post("/chats", async (req, res, next) => {
   // console.log(req.body);
   const { from, to, msg, date } = req.body;
   // console.log(from + to + msg);
   const chat_data = { from: from, to: to, msg: msg, created_at: date };
-  await Chat.insertMany([chat_data])
+  await Chat.insertMany(chat_data)
     .then(() => {
       res.redirect("/chats");
     })
     .catch((err) => {
-      res.send(err);
+      next(new ExpressError(404, "Insufficient Data Error"));
+      // res.send(err);
     });
 });
 
-app.get("/chats/:id/edit", (req, res) => {
+app.get("/chats/:id/edit", (req, res, next) => {
   // console.log(req.params);
   const { id } = req.params;
   Chat.findOne({ _id: id })
@@ -73,27 +79,33 @@ app.get("/chats/:id/edit", (req, res) => {
       });
     })
     .catch((err) => {
-      res.send(err);
+      // next(err);
+      next(err);
     });
   // res.send("Hello");
 });
 
-app.put("/chats", (req, res) => {
+app.put("/chats", (req, res, next) => {
   console.log(req.body);
   const { id, msg } = req.body;
   Chat.updateOne({ _id: id }, { $set: { msg: msg } })
     .then((m) => console.log(m))
-    .catch((err) => console.log(err));
+    .catch((err) => next(err));
   res.redirect("/chats");
 });
 
-app.delete("/:id/delete", (req, res) => {
+app.delete("/:id/delete", (req, res, next) => {
   // console.log(req.params);
   const { id } = req.params;
   Chat.findByIdAndDelete({ _id: id })
     .then((r) => console.log(r))
-    .catch((e) => console.log(e));
+    .catch((e) => next(e));
   res.redirect("/chats");
+});
+
+app.use((err, req, res, next) => {
+  const { status = 500, message = "Custom Internal Server Error " } = err;
+  res.status(status).send(message);
 });
 
 app.listen(port, () => {
